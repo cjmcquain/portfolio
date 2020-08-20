e299fdad33a69d1e93d35c975ff2f76544811c6a
Charles McQuain <charlesmcquain@gmail.com>
2020-08-19 20:26:56 -0700

    Added head and hero data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,17 +2,17 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Charles McQuain | Software Engineer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Charles McQuain Portfolio Website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Charles McQuain',
+  subtitle: "I'm a Software Engineer",
+  cta: 'About me',
 };
 
 // ABOUT DATA
