3035987b88c2a3cadc8185faa0a6a885cd1c3eb5
Charles McQuain <charlesmcquain@gmail.com>
2020-08-26 19:57:01 -0700

    Added data and removed unused vars

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,12 +2,12 @@ import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../Title/Title';
-import AboutImg from '../Image/AboutImg';
+// import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { /* img, */ paragraphOne, paragraphTwo, paragraphThree, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -27,14 +27,14 @@ const About = () => {
       <Container>
         <Title title="About Me" />
         <Row className="about-wrapper">
-          <Col md={6} sm={12}>
+          {/* <Col md={6} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
               <div className="about-wrapper__image">
                 <AboutImg alt="profile picture" filename={img} />
               </div>
             </Fade>
-          </Col>
-          <Col md={6} sm={12}>
+          </Col> */}
+          <Col md={12} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
--- src/mock/data.js
+++ src/mock/data.js
@@ -18,10 +18,12 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'I am a Software Engineer based out of Los Angeles, CA.  I am currently employed as an Application Developer at UCLA. I graduated with a Master of Science degree in Applied Computer Science. I also graduated with a Bacbelor of Science in Information Technology.',
+  paragraphTwo:
+    'Some of the technologies I am currently working with are: HTML/CSS, JavaScript, Angular, React, .NET Core, C#, and SQL Server.',
+  paragraphThree: ' ',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,12 +31,13 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    title: 'Recipe Vault',
+    info:
+      'Recipe Vault is a website where users can create recipes, share them with friends, or view recipes created by others.',
+    info2: 'Angular | .NET Core | C# | Angular Material | SQL Server | Azure | HTML/CSS/JavaScript',
+    url: 'https://recipevault20200818160828.azurewebsites.net/',
+    repo: 'https://github.com/cjmcquain/RecipeVault', // if no repo, the button will not show up
+  } /*
   {
     id: nanoid(),
     img: 'project.jpg',
@@ -52,20 +55,20 @@ export const projectsData = [
     info2: '',
     url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  }, */,
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Need to get in touch with me?',
+  btn: "Let's chat!",
+  email: 'charlesmcquain@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
+    /* {
       id: nanoid(),
       name: 'twitter',
       url: '',
@@ -74,21 +77,21 @@ export const footerData = {
       id: nanoid(),
       name: 'codepen',
       url: '',
-    },
+    }, */
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/charlesjmcquain/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/cjmcquain',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
